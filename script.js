d073871859060b32943b371aa7fdaa6b2abf6f01
Selena Nguyen <missselenah@gmail.com>
2022-03-29 16:58:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+ // like table 1 //
+
+var count1 = 9;
+var countElement1 = document.querySelector("#counttable1");
+
+console.log(countElement1);
+
+function add1() {
+    count1++;
+    countElement1.innerText = count1 +" likes(s)";
+    console.log(count1);
+}
+
+ // like table 2 //
+
+var count2 = 9;
+var countElement2 = document.querySelector("#counttable2");
+
+console.log(countElement2);
+
+function add2() {
+    count2++;
+    countElement2.innerText = count2 +" likes(s)";
+    console.log(count2);
+}
+
+ // like table 3 //
+
+var count3 = 9;
+var countElement3 = document.querySelector("#counttable3");
+
+console.log(countElement3);
+
+function add3() {
+    count3++;
+    countElement3.innerText = count3 +" likes(s)";
+    console.log(count3);
+}
